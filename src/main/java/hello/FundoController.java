0fad3fcfc80a823de4cb21fc728e3d4381cf6035
Treinamento <marco.vieira@maps.com.br>
2016-02-27 15:47:17 -0300

    Criando repositório de fundos.

--- src/main/java/hello/FundoController.java
+++ src/main/java/hello/FundoController.java
@@ -2,8 +2,6 @@ package hello;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,31 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FundoController {
 
-	private static Map<String, CadastroFundo> fundos = new HashMap<String, CadastroFundo>();
-
-	static {
-		add(new CadastroFundo("1", "FUNDO 1", 1.11d));
-		add(new CadastroFundo("2", "FUNDO 2", 2.22d));
-		add(new CadastroFundo("3", "FUNDO 3", 3.33d));
-		add(new CadastroFundo("4", "FUNDO 4", 4.44d));
-		add(new CadastroFundo("5", "FUNDO 5", 5.55d));
-	}
-
-	static CadastroFundo add(CadastroFundo cad) {
-		fundos.put(cad.getCnpj(), cad);
-		return cad;
-	}
+	private FundoRepository fundoRepository;
 
 	private QuoteService quoteService;
-	
+
 	@Autowired
-	public FundoController(QuoteService quoteService) {
+	public FundoController(FundoRepository fundoRepository, QuoteService quoteService) {
+		this.fundoRepository = fundoRepository;
 		this.quoteService = quoteService;
 	}
-	
+
 	@RequestMapping("/fundo/cadastro")
 	public Collection<CadastroFundo> find(@RequestParam(required = false) String cnpj) {
-		return cnpj == null ? fundos.values() : Arrays.asList(fundos.get(cnpj));
+		return cnpj == null ? fundoRepository.findAll() : Arrays.asList(fundoRepository.find(cnpj));
+	}
+
+	// curl --data '{"cnpj" : "6", "nome" : "FUNDO 6", "valorCota" : "6.66"}'
+	// -X POST -H 'Content-Type:application/json'
+	// http://localhost:8080/fundo/cadastro/novo
+	@RequestMapping(method = RequestMethod.POST, path = "/fundo/cadastro/novo")
+	public CadastroFundo create(@RequestBody CadastroFundo cad) {
+		System.out.println("criando " + cad);
+		fundoRepository.add(cad);
+		return cad;
 	}
 
 	@RequestMapping("/fundo/cadastro/hint")
@@ -49,15 +45,5 @@ public class FundoController {
 		String quoteValue = quote.getValue().getQuote();
 		CadastroFundo novoCad = new CadastroFundo(idQuote.toString(), quoteValue, idQuote * idQuote);
 		return novoCad;
-	}
-	
-	// curl --data '{"cnpj" : "6", "nome" : "FUNDO 6", "valorCota" : "6.66"}'
-	// -X POST -H 'Content-Type:application/json'
-	// http://localhost:8080/fundo/cadastro/novo
-	@RequestMapping(method = RequestMethod.POST, path = "/fundo/cadastro/novo")
-	public CadastroFundo create(@RequestBody CadastroFundo cad) {
-		System.out.println("criando " + cad);
-		add(cad);
-		return cad;
 	}
 }
---
+++ src/main/java/hello/FundoRepository.java
@@ -0,0 +1,35 @@
+package hello;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FundoRepository {
+
+	private static Map<String, CadastroFundo> fundos = new HashMap<String, CadastroFundo>();
+
+	public FundoRepository() {
+		add(new CadastroFundo("1", "FUNDO 1", 1.11d));
+		add(new CadastroFundo("2", "FUNDO 2", 2.22d));
+		add(new CadastroFundo("3", "FUNDO 3", 3.33d));
+		add(new CadastroFundo("4", "FUNDO 4", 4.44d));
+		add(new CadastroFundo("5", "FUNDO 5", 5.55d));
+	}
+
+	
+	public CadastroFundo add(CadastroFundo cad) {
+		fundos.put(cad.getCnpj(), cad);
+		return cad;
+	}
+
+	public Collection<CadastroFundo> findAll() {
+		return fundos.values();
+	}
+
+	public CadastroFundo find(String cnpj) {
+		return fundos.get(cnpj);
+	}
+}
